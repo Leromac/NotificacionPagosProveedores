bec177fe79eeb0e45b95eaa79dc818767e1d21f2
leromac <leromac@hotmail.com>
2022-06-03 23:24:35 -0500

    Se corrigen errores en el setting.py y se corre el servidor por primera vez.

--- npp/npp/settings.py
+++ npp/npp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Local apps
-    'suplier',
+    'supplier',
     'log',
     'notification',
 ]
@@ -126,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
+MEDIA_URL = '/upload/'
