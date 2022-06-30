87fde70b4802619f35878f53282ec95e6c413d5f
leromac <leromac@hotmail.com>
2022-06-30 14:23:50 -0500

    Agregar supplier y buscar supplier estan funcionando, se agregan datos a la base de datos, se consultan y muestran en pantalla

--- npp/supplier/views.py
+++ npp/supplier/views.py
@@ -1,3 +1,4 @@
+import this
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 import email
@@ -18,41 +19,45 @@ def addSupplier(request):
     
     if request.method == 'POST':
         try:
-            existSupplier = (supplier.objects.filter(taxIdentificationNumber=form['taxIdentificationNumber'].value()).filter(email=form['email'].value()).exists())
             form = addSupplierForm(request.POST)
-
-            if(existSupplier):
+            
+            if(supplier.objects.filter(taxIdentificationNumber=form['taxIdentificationNumber'].value()).filter(email=form['email'].value()).exists()==False):
                 form.save()
-                messages.success(request, "Email para notificacion agregado correctamente %s" %existSupplier)
+                messages.success(request, "Email (%s) para notificacion agregado correctamente" %form['email'].value())
             else:
                 messages.error(request, "El Email (%s) ya se encuentra agregado para este proveedor." %form['email'].value())
+
+            return render(
+                request,
+                'getSupplier.html',
+                {'listEmailSupplier': supplier.objects.filter(taxIdentificationNumber=form['taxIdentificationNumber'].value())}
+            )
         except BaseException as err: 
-            messages.error(request, "Hubo un error al guardar el artículo \n %s " %err)
-        #return redirect('getIndividualSupplier', form['taxIdentificationNumber'].value())
-        #getIndividualSupplier(getIndividualSupplier, form['taxIdentificationNumber'].value())
-    return render(
-        request,
-        'addSupplier.html',
-        {'formAddSupplier': form}
-    )
+            messages.error(request, "Hubo un error al guardar el Email \n %s " %err)
+    else:    
+        return render(
+            request,
+            'addSupplier.html',
+            {'formAddSupplier': form}
+        )
     
 def searchSupplier(request):
     form = searchSupplierForm()
     
     if request.method == 'POST':
-        form = searchSupplierForm(request.POST)
-    
-    return render(
-        request,
-        'searchSupplier.html',
-        {'formSearchSupplier': form}
-    )
-    
-def getIndividualSupplier(request, taxIdentificationNumber):
-    individualSupplierList = supplier.objects.filter(taxIdentificationNumber=taxIdentificationNumber)
-    
-    return render(
-        request,
-        'getSupplier.html',
-        {'dataGetSupplier': individualSupplierList}
-    )
+        try:
+            form = searchSupplierForm(request.POST)
+               
+            return render(
+                request,
+                'getSupplier.html',
+                {'listEmailSupplier': supplier.objects.filter(taxIdentificationNumber=form['taxIdentificationNumber'].value())}
+            )
+        except BaseException as err: 
+            messages.error(request, "Error al consultar los datos \n %s " %err)
+    else:
+        return render(
+            request,
+            'searchSupplier.html',
+            {'formSearchSupplier': form}
+        )
