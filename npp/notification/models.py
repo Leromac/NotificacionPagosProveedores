3a0f8ff571fa07d2b18817ce8aad9e744b758605
leromac <leromac@hotmail.com>
2022-07-04 12:19:53 -0500

    Se recibe archivo .csv para su procesamiento

--- npp/notification/models.py
+++ npp/notification/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class notification(models.Model):
     baseFile = models.FileField(name="baseFile", max_length=500)
-    #uploadTo = models.FileField(upload_to='')
-    #creationDate = models.DateTimeField(auto_now_add=True)
+    uploadTo = models.FileField(upload_to='')
+    creationDate = models.DateTimeField(auto_now_add=True)
     
 
 class notificationFields(models.Model):
--- npp/notification/views.py
+++ npp/notification/views.py
@@ -12,21 +12,22 @@ def notificationIndex(request):
             form = sendNotificationForm(request.POST, request.FILES)
 
             if form.is_valid():
-                newFiletoProcess = notification(baseFile = request.FILES['uploadTo'])
-                newFiletoProcess = notification(uploadTo = request.FILES['uploadTo'])
-
+                newFiletoProcess = notification(baseFile = request.FILES['baseFile'])
+                #newFiletoProcess = notification(uploadTo = request.FILES['baseFile'])
+                
                 newFiletoProcess.save(form)
 
                 messages.success(request, "Archivo recibido para procesamiento %s" %newFiletoProcess.baseFile)
                 #procesar_archivo(request.FILES['docfile'])   
 
-            return render(
-                request,
-                'notificationIndex.html',
-                {'formSendNotification': form}
-            )
         except BaseException as err: 
             messages.error(request, "Error al consultar los datos \n %s " %err)
+            
+        return render(
+            request,
+            'notificationIndex.html',
+            {'formSendNotification': form}
+        )
     else:
         return render(
         request,
