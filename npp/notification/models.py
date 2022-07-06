348cd24fcba86738471b5c80fb7d1e0efc3c61a7
leromac <leromac@hotmail.com>
2022-07-06 14:39:23 -0500

    Envio correos de notificacion correctamente, corregidos errores menores

--- npp/notification/models.py
+++ npp/notification/models.py
@@ -2,7 +2,10 @@ from asyncio.windows_events import NULL
 from django.db import models
 import csv
 import random
+from datetime import datetime
+from django.template.loader import get_template
 from django.conf import settings
+from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from supplier.models import supplier
 
@@ -18,44 +21,31 @@ class notificationContent(models.Model):
     accountNumber = models.CharField(name="accountNumber", max_length=50)
     transactionDate = models.DateField(name="transactionDate")
     transactionDocumentType = models.CharField(name="transactionDocumentType", max_length=100)
-    icaRetention = models.FloatField(name="icaRetention")
-    ivaRetention = models.FloatField(name="ivaRetention")
-    rentaRetention = models.FloatField(name="rentaRetention")
-    amountPaid = models.FloatField(name="amountPaid")
-    totalValueInvoice = models.FloatField(name="totalValueInvoice")
+    icaRetention = models.CharField(name="icaRetention", max_length=20)
+    ivaRetention = models.CharField(name="ivaRetention", max_length=20)
+    rentaRetention = models.CharField(name="rentaRetention", max_length=20)
+    amountPaid = models.CharField(name="amountPaid", max_length=20)
+    totalValueInvoice = models.CharField(name="totalValueInvoice", max_length=20)
     note = models.CharField(name="note", max_length=1000)
 
-    def __init__(self, customId, taxIdentificationNumber, accountNumber, transactionDate, transactionDocumentType, icaRetention, ivaRetention, rentaRetention, amountPaid, totalValueInvoice, note):
-        self.customId = customId
-        self.taxIdentificationNumber = taxIdentificationNumber
-        self.accountNumber = accountNumber
-        self.transactionDate = transactionDate
-        self.transactionDocumentType = transactionDocumentType
-        self.icaRetention = icaRetention
-        self.ivaRetention = ivaRetention
-        self.rentaRetention = rentaRetention
-        self.amountPaid = amountPaid
-        self.totalValueInvoice = totalValueInvoice
-        self.note = note
-
 def readFile (fileName):
-    notificationCustomId = random()
     controlList = []
-    #sendList = []
-    nc = notificationContent()
-
+    
     try:
+        notificationCustomId = str(random.random())
+        notificationCustomId = notificationCustomId.lstrip("0.")
+        
         with open('upload\%s' %fileName) as csvarchivo:
             entrada = csv.reader(csvarchivo)
             next(entrada, None)
-                       
-            #textoEnvio = ""
 
             for reg in entrada:
+                nc = notificationContent()
+
                 nc.customId = notificationCustomId
                 nc.taxIdentificationNumber = reg[0]
                 nc.accountNumber = reg[1]
-                nc.transactionDate = reg[2]
+                nc.transactionDate = datetime.strptime(reg[2], '%d/%m/%Y')
                 nc.transactionDocumentType = reg[3]
                 nc.icaRetention = reg[4]
                 nc.ivaRetention = reg[5]
@@ -66,42 +56,23 @@ def readFile (fileName):
 
                 nc.save()
 
-                #sendList.append(nf)
-                
                 if reg[0] not in controlList:
                     controlList.append(reg[0])
-                    #textoEnvio += "<tr><td ALIGN=right>" + reg[3] + "</td><td ALIGN=right>" + reg[8] + "</td><td ALIGN=right>" + reg[4] + "</td><td ALIGN=right>" + reg[5] + "</td><td ALIGN=right>" + reg[6] + "</td><td ALIGN=right>" + reg[7] + "</td><td ALIGN=left>" + reg[9] + "</td></tr>"
-                #else:
-                    #if textoEnvio:
-                    #    textoEnvio += "</table><br></br><p>Agradecemos distribuir esta informacion al personal de su empresa que le puedan ser util estos datos.</p>"
-                    #    textoEnvio += "<br></br><footer><strong>POR FAVOR NO RESPONDER A ESTA CUENTA DE CORREO, NADIE MONITOREA ESTOS MENSAJES Y SE ELIMINAN AUTOMATICAMENTE.</strong></footer>"
-                    #    listaEnvio.append(textoEnvio)
-                    #    textoEnvio = ""
-                
-                    #controlList.append(reg[0])
-                    #textoEnvio += "<h3>Cordial Saludo.</h3>"
-                    #textoEnvio += "<br></br><h3>Industrias Astivik le informa, que se realizo una transferencia el dia " + reg[2] + " a su cuenta bancaria No " + reg[1] + "</h3>"
-                    #textoEnvio += "<br></br><p>Las facturas pagadas fueron las siguientes:</p>"
-                    #textoEnvio += '<br></br><table BORDER bordercolor="black" CELLPADDING=10 CELLSPACING=0><tr><th>No FACTURA</th><th>VALOR NETO FACTURA</th><th>VALOR RET ICA</th><th>VALOR RET IVA</th><th>VALOR RET RENTA</th><th>VALOR PAGADO</th><th>NOTA</th></tr>'
-                    #textoEnvio += "<tr><td>" + reg[3] + "</td><td ALIGN=right>" + reg[8] + "</td><td ALIGN=right>" + reg[4] + "</td><td ALIGN=right>" + reg[5] + "</td><td ALIGN=right>" + reg[6] + "</td><td ALIGN=right>" + reg[7] + "</td><td ALIGN=left>" + reg[9] + "</td></tr>"
-           
-            #textoEnvio += "</table><br></br></p>Agradecemos distribuir esta informacion al personal de su empresa que le puedan ser util estos datos.</p>"
-            #textoEnvio += "<br></br><footer><strong>POR FAVOR NO RESPONDER A ESTA CUENTA DE CORREO, NADIE MONITOREA ESTOS MENSAJES Y SE ELIMINAN AUTOMATICAMENTE.</strong></footer>"
-            #listaEnvio.append(textoEnvio)
-            sentNotification(notificationCustomId)
+                    
+            sentNotification(notificationCustomId, controlList)
     except Exception as ex:
-            print ("Error al leer el archivo .csv \n %s " % (ex))
+        print ("Error al leer el archivo .csv %s " % (ex))
     
     return True
     
-def sentNotification(notificationCustomId):
+def sentNotification(notificationCustomId, supplierTaxIdentificationNumberList):
     try:
-        supplierTaxIdentificationNumberList =  notificationContent.objects.filter(customId="%s" %notificationCustomId).distinct('taxIdentificationNumber')
+        #supplierTaxIdentificationNumberList =  notificationContent.objects.filter(customId="%s" %notificationCustomId).distinct('taxIdentificationNumber')
          
         for i in range(len(supplierTaxIdentificationNumberList)):
             operationList = supplier.objects.filter(taxIdentificationNumber="%s" % supplierTaxIdentificationNumberList[i])
             contentList = notificationContent.objects.filter(customId="%s" %notificationCustomId).filter(taxIdentificationNumber="%s" %supplierTaxIdentificationNumberList[i])
-            #print ("enviado A %s " % (listaControl[i]))
+            
             
             x=0
             
@@ -111,17 +82,18 @@ def sentNotification(notificationCustomId):
                     #noEnviado+= "</br>"+(filas[x].nit)+" -- "+(filas[x+1].nombre)
                     #print ("NO SE PUDO ENVIAR CORREO A %s " % ((filas[x].nit)+" -- "+(filas[x+1].nombre)))
                 else:
-                    if(filas[x+2].email != ""):
-                        y+=1
+                    if(filas.email != ""):
+                        #+=1
                         try:
-                            sendMail(contentList, filas[x+2].email)
+                            sendMail(contentList, filas.email)
                             #models.envioCorreo("no-reply@astivik.com", (filas[x+2].email), listaEnvio[i])
                             #textoEnvioControl += "<tr><td ALIGN=center>" + str(y) + "</td><td ALIGN=left>" + (filas[x+1].nombre) + "</td><td ALIGN=left>" + (filas[x+2].email) + "</td></tr>"
                             #print ("enviado A %s " % ((filas[x].nit)+" -- "+(filas[x+1].nombre)))
                         except Exception as ex:
                             print ("Error al enviar notificacion %s %s" % ((supplierTaxIdentificationNumberList[i]), ex))
                     else:
-                        noEnviado+= "</br>"+(filas[x].nit)+" -- "+(filas[x+1].nombre+"</br>")
+                        pass
+                        #noEnviado+= "</br>"+(filas[x].nit)+" -- "+(filas[x+1].nombre+"</br>")
                                         
                 x+=3
     except Exception as ex:
@@ -131,21 +103,24 @@ def sentNotification(notificationCustomId):
 
 
 def sendMail(body, to):
-    subject = 'Astivik S.A. -- Notificación Pagos Realizados.'
-    template = get_template('templates/emailMessage.html')
+    try:
+        subject = 'Astivik S.A. -- Notificación Pagos Realizados.'
+        template = get_template('emailMessage.html')
 
-    content = template.render({
-        'NotificacionMessage': body,
-    })
+        content = template.render({
+            'NotificacionMessage': body,
+        })
 
-    message = EmailMultiAlternatives(subject,
-                                    '',
-                                    settings.EMAIL_HOST_USER,
-                                    to=[
-                                        to
-                                    ])
+        message = EmailMultiAlternatives(subject,
+                                        '',
+                                        settings.EMAIL_HOST_USER,
+                                        to=[
+                                            to
+                                        ])
 
-    message.attach_alternative(content, 'text/html')
-    message.send()
+        message.attach_alternative(content, 'text/html')
+        message.send()
+    except Exception as ex:
+        print ("Error al enviar correo \n %s " % (ex))
 
     return True
